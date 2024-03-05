7bb6d944c57830c6219e45e5011c5474d22a893c
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-03-05 23:33:51 +0530

    fundamentals section

--- src/components/PerformanceSection.tsx
+++ src/components/PerformanceSection.tsx
@@ -1,9 +1,11 @@
+import { IoMdInformationCircle } from "react-icons/io";
+
 function PerformanceSection() {
   return (
-    <div className="bg-white mt-4 rounded-lg p-6 h-96">
+    <div className="bg-white mt-4 rounded-lg p-6 h-max">
       <div>
         <div className="text-2xl font-semibold text-[#0F1629]">Performance</div>
-        <div className="py-2 mt-2">
+        <div className="py-4 mt-2">
           <div className="flex justify-between ">
             <div className="text-start">
               <div className="text-sm text-[#44475B] font-normal p-1">
@@ -54,6 +56,113 @@ function PerformanceSection() {
               </div>
               <div className="text-[#44475B] text-lg font-medium p-1">
                 49,743.83{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <div className="flex items-center">
+            <div className="text-[#44475B] font-semibold text-xl">
+              Fundamentals
+            </div>
+            <div className="">
+              <IoMdInformationCircle className="text-[#ABB9BF] text-lg ml-2" />
+            </div>
+          </div>
+          <div className="flex mb-8">
+            <div className="w-1/2 mr-10 mt-3  ">
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  Bitcoin Price
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  $16,815.46
+                </div>
+              </div>
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  24h Low / 24h High{" "}
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  $16,382.07 / $16,874.12
+                </div>
+              </div>
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  7d Low / 7d High
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  $16,382.07 / $16,874.12
+                </div>
+              </div>
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  Trading Volume
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  $23,249,202,782
+                </div>
+              </div>
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  Market Cap Rank
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  #1
+                </div>
+              </div>
+            </div>
+            <div className="w-1/2 ml-10 mt-3">
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  Market Cap
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  $323,507,290,047
+                </div>
+              </div>
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  Market Cap Dominance
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  38.343%
+                </div>
+              </div>
+              <div className="flex justify-between py-5 border-b-2 border-[#D3E0E6]">
+                <div className="text-[#768396] text-sm font-semibold">
+                  Volume / Market Cap
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4">
+                  0.0718
+                </div>
+              </div>
+              <div className="flex justify-between py-3 border-b-2 border-[#D3E0E6] items-center">
+                <div className="text-[#768396] text-sm font-semibold ">
+                  All-Time High
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4 -p-2">
+                  <div className="text-end">
+                    $69,044.77 <span className="text-red-500">-75.6%</span>
+                  </div>
+                  <div className="text-xs font-normal">
+                    Nov 10, 2021 (about 1 year)
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between py-3 border-b-2 border-[#D3E0E6] items-center">
+                <div className="text-[#768396] text-sm font-semibold">
+                  All-Time Low
+                </div>
+                <div className="text-[#111827] text-sm font-semibold mr-4 -p-2">
+                  <div className="text-end">
+                  $67.81 <span className="text-green-500">-24729.1%</span>
+                  </div>
+                  <div className="text-xs font-normal">
+                  Jul 06, 2013 (over 9 years)
+                  </div>
+                </div>
               </div>
             </div>
           </div>
